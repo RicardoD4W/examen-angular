bd0a5981aeb2ce5720f049bd1867484330f93800
ricardod4w <ricardo.rodriguez@fernando3martos.com>
2022-12-12 10:25:25 +0100

    add

--- src/app/weather/result/result.component.ts
+++ src/app/weather/result/result.component.ts
@@ -12,6 +12,12 @@ export class ResultComponent implements OnInit {
   constructor(private weatherService : WeatherserviceService) { }
 
   ruta() : string{
+    if(this.busqueda.list[0].weather[0].icon.includes('n')){
+      document.querySelector('.special')?.classList.remove('from-blue-300');
+      document.querySelector('.special')?.classList.remove('shadow-blue-300');
+      document.querySelector('.special')?.classList.add('from-orange-300');
+      document.querySelector('.special')?.classList.add('shadow-orange-300');
+    }
     return 'http://openweathermap.org/img/wn/'+ this.busqueda.list[0].weather[0].icon +'@2x.png';
   } 
 
@@ -20,6 +26,7 @@ export class ResultComponent implements OnInit {
   }
 
   get busqueda() {
+
         return this.weatherService.getBusquedas();
 
   }
