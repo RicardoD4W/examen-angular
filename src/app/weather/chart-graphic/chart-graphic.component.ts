66e03690ff5d394fe2df67c49a27b613fd7420b0
ricardod4w <ricardo.rodriguez@fernando3martos.com>
2022-12-15 21:45:34 +0100

    space

--- src/app/weather/chart-graphic/chart-graphic.component.ts
+++ src/app/weather/chart-graphic/chart-graphic.component.ts
@@ -11,7 +11,7 @@ import { WeatherserviceService } from '../servicios/weatherservice.service';
   styleUrls: ['./chart-graphic.component.css']
 })
 export class ChartGraphicComponent {
-
+ 
 
   constructor(
     private weatherService : WeatherserviceService, 
