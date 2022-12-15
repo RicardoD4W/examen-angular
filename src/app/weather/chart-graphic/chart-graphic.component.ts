c337ca0e5fa0fc723197bf1192de3ab1aa928469
ricardod4w <ricardo.rodriguez@fernando3martos.com>
2022-12-15 21:43:43 +0100

    add

--- src/app/weather/chart-graphic/chart-graphic.component.ts
+++ src/app/weather/chart-graphic/chart-graphic.component.ts
@@ -22,6 +22,16 @@ export class ChartGraphicComponent {
 
   tiempo! : Tiempo;
   public chart: any;
+  public options: any = {
+    aspectRatio:2.5,
+    tooltips: {
+      // Disable the on-canvas tooltip
+      enabled: true,
+
+    }
+    
+    
+  };
 
   fechas : string[] = [];
   temperaturas : string[] = [];
@@ -40,24 +50,29 @@ export class ChartGraphicComponent {
 
 
   createChart(){    
+    Chart.defaults.scales.linear.min = -10;
+    Chart.defaults.scales.linear.max = 45;
+
     this.activeRouting.params.subscribe(( {nombre} )=>{
       this.weatherService.peticionDetails(nombre).subscribe((respose)=>{
         this.tiempo = respose;
         for(let i = 0; i< 8 ; i++){
-          this.fechas.push(this.tiempo.list[i].dt_txt.toString());
-          this.temperaturas.push(this.tiempo.list[i].main.temp.toString());
-          console.log(this.temperaturas)
+          this.fechas.push(this.tiempo.list[i].dt_txt.toString().substring(10,16));
+          this.temperaturas.push(((this.tiempo.list[i].main.temp -273)  ).toString());
+          console.log(this.tiempo)
         }
+
+        
         this.chart = new Chart("MyChart", {
           type: 'line', //this denotes tha type of chart
           data: {// values on X-Axis
             labels: [...this.fechas], 
              datasets: [
               {
-                label: "Sales",
-                data: ['54.2', '542', '536', '327', '17',
-                '0.00', '538', '541'],
-                backgroundColor: 'blue'
+                label: "Temperatura - ºC",
+                data: [...this.temperaturas],
+                backgroundColor: 'blue',
+                pointRadius : 5 
               },
               {
                 label: "Profit",
@@ -67,9 +82,7 @@ export class ChartGraphicComponent {
               }  
             ]
           },
-          options: {
-            aspectRatio:2.5
-          }
+          options: this.options
           
         });
       })
